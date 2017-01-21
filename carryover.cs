efadb69eb0ba004ba38bb90b5c338caab82c1f4a
Tom Lucas <tlucas03@dcs.bbk.ac.uk>
2017-01-21 19:22:21 +0000

    added classes

---
+++ carryover.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class carryover : MonoBehaviour {
+
+    public Canvas myCanvas;
+
+    public Text myText;
+    
+    public static int yourscore;
+
+    static private carryover myPlayer;
+
+    public static carryover getPlayer()
+    {
+
+        return myPlayer;
+
+    }
+
+    public void thescore(int score)
+    {
+        yourscore = score + 10;
+    }
+
+    public int getScore()
+    {
+        return yourscore;
+    }
+
+    void Awake()
+    {
+        /*if (myPlayer == null)
+        {
+            myPlayer = this;
+        }
+        else if (myPlayer != null)
+        {
+            Destroy(gameObject);
+        }*/
+        if (SceneManager.GetActiveScene().name == "victory")
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            myText.text = "Your score is: " + getScore();
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //myText.text = "Your score is: " + getScore();
+    }
+}
---
+++ scrollwaves.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollwaves : MonoBehaviour {
+    private Vector2 myDirection;
+    private float myX;
+    private float myY;
+    private bool up;
+    private bool down;
+    public float speed;
+    private int range;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        up = true;
+        down = false;
+        myDirection = new Vector2(0.0f, myY);
+    }
+
+    void FixedUpdate()
+    {
+        //Controls the enemy object's movements - moves up and down within the range set by the public range value.
+        if (range > 50 || up)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(1.0f, 1.0f) * speed;
+            down = false;
+            up = true;
+            range--;
+        }
+        if (range <= -50 || down)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(1.0f, 1.0f) * -speed;
+            myY = 100.0f;
+            up = false;
+            down = true;
+            range++;
+        }
+    }
+}
