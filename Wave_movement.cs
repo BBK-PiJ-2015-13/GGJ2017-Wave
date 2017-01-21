00144e54b9f99288a86a3201de12676bf623857e
Tom Lucas <tlucas03@dcs.bbk.ac.uk>
2017-01-21 19:14:14 +0000

    update1

--- Wave_movement.cs
+++ Wave_movement.cs
@@ -12,30 +12,61 @@ public class Wave_movement : MonoBehaviour {
     public float minimum;
     public Slider heightSlider;
     public float maxheight;
+    public float up;
+    public Vector2 baseWave;
+    public float baseDistance;
+    private Transform ocean;
+    public float sealevel;
+    public float initialV;
 
     // Use this for initialization
-    void Start () {
-        maxheight = 100;
+    void Start() {
+        up = transform.position.y;
+        //transform.localScale = baseWave;
+        distance = baseDistance * 10;
+        ocean = GameObject.Find("waterline").transform;
+        sealevel = ocean.transform.position.x;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+        if (sealevel < 0)
+        {
+            sealevel = sealevel * -1;
+        }
+        maxheight = 10;//sealevel + (sealevel / 100 * 40);
         heightSlider.maxValue = maxheight;
-	}
+        heightSlider.value = transform.lossyScale.y; //GetComponent<BoxCollider2D>().size.y;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Vector2 Rightmovement = new Vector2(-slowdown, 0);
-            GetComponent<Rigidbody2D>().velocity = Rightmovement / speed;
-            distance--;
+            if (slowdown > 1)
+            {
+                slowdown -= Time.deltaTime;
+            }
+            
+            Vector2 Rightmovement = new Vector2(-slowdown, 0.0f);
+            GetComponent<Rigidbody2D>().velocity = Rightmovement *10 / speed;
+            if (distance > 0)
+            {
+                distance--;
+                //GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+                heightSlider.value = distance / 10;
+            }
+            
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             slowdown += Time.deltaTime;
-            Vector2 Rightmovement = new Vector2(-slowdown, 0);
+            Vector2 Rightmovement = new Vector2(-slowdown, 0.0f);
             GetComponent<Rigidbody2D>().velocity = Rightmovement * speed;
             distance++;
+            heightSlider.value = distance / 10;
+            //GetComponent<Rigidbody2D>().velocity = transform.up * -speed;
         }
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
             slowdown = minimum;
         }
@@ -43,7 +74,10 @@ public class Wave_movement : MonoBehaviour {
 
         if (distance >= 0)
         {
-            //transform.localScale = new Vector2(distance, distance);
+
+            //transform.localScale = new Vector2(distance /10, distance /10);
+
+
         }
         /*float moveHorizontal = Input.GetAxis("Horizontal");
         distance += moveHorizontal /10;
@@ -55,13 +89,12 @@ public class Wave_movement : MonoBehaviour {
         }*/
 
 
-        if (distance > 100)
+        if (heightSlider.value == heightSlider.maxValue || heightSlider.value <= 0)
         {
             //Destroy(this.gameObject);
             //SceneManager.LoadScene("gameover");
         }
-
-        heightSlider.value = distance;
+       
     }
 
     public void OnCollisionEnter2D(Collision2D other)
@@ -70,9 +103,18 @@ public class Wave_movement : MonoBehaviour {
         {
             //transform.localScale = new Vector2(7.5f, 7.5f);
         }
-        if (other.collider.tag == "beach")
+        if (other.collider.tag == "beach") { 
+            if (heightSlider.value < heightSlider.maxValue / 2)
+            {
+                SceneManager.LoadScene("feeble");
+            }
+        else
         {
-            SceneManager.LoadScene("victory");
+                carryover x = other.gameObject.GetComponent("carryover") as carryover;
+                x.thescore((int) distance + (int) slowdown);
+                //x.yourscore = (int)distance + (int)slowdown;
+                SceneManager.LoadScene("victory");
+        }
         }
     }
 }
