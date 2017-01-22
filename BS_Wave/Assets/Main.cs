b66a2264992fa91aa678df9a32afe3114b62ad0b
U-Arsen-PC\Arsen <hadzabadza@yandex.ru>
2017-01-22 14:07:59 +0000

    Last?

--- BS_Wave/Assets/Main.cs
+++ BS_Wave/Assets/Main.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
+
+	public int score;
+	public Text scoreTxt;
+	public bool showScore;
+	public Slider sl;
+	public Image handle;
 
 	public GameObject waveClass;
 	// Use this for initialization
 	void Start () {
 		GameObject wave = Instantiate (waveClass, new Vector2(transform.position.x,transform.position.y), transform.rotation);
 		wave.GetComponent<WaveScript>().cam = gameObject;
+		wave.GetComponent<WaveScript> ().sl = sl;
+		wave.GetComponent<WaveScript> ().handle = handle;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (showScore) {
+			scoreTxt.text = "Score: " + score;
+			scoreTxt.enabled = true;
+		} else {
+			scoreTxt.enabled = false;
+		}
 	}
 }
